852c4b3de9bd33428458b3e8e2b7b33585b4717e
Lars Vonk <lars.vonk@despar.nl>
2025-02-09 11:43:39 +0100

    chore: improved get shops flow

--- filter_shops_platform.py
+++ filter_shops_platform.py
@@ -12,61 +12,59 @@ TECHNOLOGIES = [
     "WordPress",
 ]
 
-date_str = datetime.datetime.today().strftime("%Y-%m-%d")
-data_file_str = f"data/shops_{date_str}.csv"
 
-with open(data_file_str, "w") as f:
-    f.write("product,company,website,technology\n")
+def filter_shops_platform():
+    date_str = datetime.datetime.today().strftime("%Y-%m-%d")
+    data_file_str = f"data/shops_{date_str}.csv"
 
+    with open(data_file_str, "w") as f:
+        f.write("product,company,website,technology\n")
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+    }
 
+    def check_shops(websites):
+        result = []
 
-def check_shops(websites):
-    result = []
+        for website in websites:
+            try:
+                product = website["product"]
+                company = website["company"]
+                website_url = website["website"]
 
-    for website in websites:
-        try:
-            product = website["product"]
-            company = website["company"]
-            website_url = website["website"]
+                # Get HTML from website
+                response = requests.get(website_url, headers=headers, timeout=5)
 
-            # Get HTML from website
-            response = requests.get(website_url, headers=headers, timeout=5)
-
-            # Check if website is online
-            if response.status_code != 200:
-                continue
-
-            # Check if website is of one of the technologies
-            for technology in TECHNOLOGIES:
-                if not technology.lower() in response.text.lower():
+                # Check if website is online
+                if response.status_code != 200:
                     continue
 
-                result.append(
-                    {
-                        "product": product,
-                        "company": company,
-                        "website": website_url,
-                        "technology": technology,
-                    }
-                )
+                # Check if website is of one of the technologies
+                for technology in TECHNOLOGIES:
+                    if not technology.lower() in response.text.lower():
+                        continue
 
-                with open(data_file_str, "a") as f:
-                    f.write(f"{product},{company},{website_url},{technology}\n")
+                    result.append(
+                        {
+                            "product": product,
+                            "company": company,
+                            "website": website_url,
+                            "technology": technology,
+                        }
+                    )
 
-                print(f"Found {technology} on {website_url} ({len(result)})")
-                break
-        except Exception as e:
-            print(f"Error: {e}")
-            continue
+                    with open(data_file_str, "a") as f:
+                        f.write(f"{product},{company},{website_url},{technology}\n")
 
-    return result
+                    print(f"Found {technology} on {website_url} ({len(result)})")
+                    break
+            except Exception as e:
+                print(f"Error: {e}")
+                continue
 
+        return result
 
-if __name__ == "__main__":
     # Load shops from daily leads list
     leads_file_str = f"data/leads_{date_str}.csv"
     leads = csv.DictReader(open(leads_file_str))
@@ -74,4 +72,8 @@ if __name__ == "__main__":
     print(f"Filtering initial leads by platform...")
 
     # Filter shops
-    shops = check_shops(leads)
+    check_shops(leads)
+
+
+if __name__ == "__main__":
+    filter_shops_platform()
--- get_shops.py
+++ get_shops.py
@@ -8,103 +8,133 @@ from playwright.sync_api import sync_playwright
 # Google search URL
 GOOGLE_SEARCH_URL = "https://www.bing.com/search"
 
-date_str = datetime.datetime.today().strftime("%Y-%m-%d")
-data_file_str = f"data/leads_{date_str}.csv"
+FORBIDDEN_SHOPS = [
+    "bol.com",
+    "amazon.nl",
+    "coolblue.nl",
+    "mediamarkt.nl",
+    "marktplaats.nl",
+    "store.steampowered.com",
+    "beslist.nl",
+    "google.com",
+    "ebay.nl",
+]
 
-with open(data_file_str, "w") as data_file:
-    data_file.write("product,company,website\n")
 
+def get_shops():
+    date_str = datetime.datetime.today().strftime("%Y-%m-%d")
+    data_file_str = f"data/leads_{date_str}.csv"
 
-# Function to perform Google search
-def google_search(page, product):
-    product = product.replace(" ", "+").lower()
+    with open(data_file_str, "w") as data_file:
+        data_file.write("product,company,website\n")
 
-    search_url = f"{GOOGLE_SEARCH_URL}?q={product}+kopen"
-
-    page_count = 0
-
-    result = []
     website_url_history = []
 
-    # Search first 5 pages
-    while page_count < 5:
-        try:
-            page_search_url = f"{search_url}&first={page_count * 10}"
+    # Function to perform Google search
+    def google_search(page, product):
+        product = product.replace(" ", "+").lower()
 
-            page.goto(page_search_url)
-            page.wait_for_load_state("networkidle")
+        search_url = f"{GOOGLE_SEARCH_URL}?q={product}+kopen"
 
-            # Get all search results
-            websites = page.query_selector_all("a.tilk > div.tptxt")
+        page_count = 0
 
-            for website in websites:
-                website_url = website.query_selector("div.tpmeta > div.b_attribution")
-                website_name = website.query_selector("div.tptt")
+        result = []
 
-                website_url_text = website_url.inner_text()
-                website_name_text = website_name.inner_text()
+        # Search first 5 pages
+        while page_count < 5:
+            try:
+                page_search_url = f"{search_url}&first={page_count * 10}"
 
-                # Get all test before first white space
-                website_url_text = website_url_text.split(" ")[0]
+                page.goto(page_search_url)
+                page.wait_for_load_state("networkidle")
 
-                # Parse URL to find origin
-                parsed_url = urlparse(website_url_text)
+                # Get all search results
+                websites = page.query_selector_all("a.tilk > div.tptxt")
 
-                if not parsed_url.netloc.endswith(
-                    ".nl"
-                ) and not parsed_url.netloc.endswith(".com"):
-                    continue
+                for website in websites:
+                    website_url = website.query_selector(
+                        "div.tpmeta > div.b_attribution"
+                    )
+                    website_name = website.query_selector("div.tptt")
 
-                if website_url_text in website_url_history:
-                    continue
+                    website_url_text = website_url.inner_text()
+                    website_name_text = website_name.inner_text()
 
-                website_url_history.append(website_url_text)
-                result.append(
-                    {
-                        "product": product,
-                        "company": website_name_text,
-                        "website": website_url_text,
-                    }
-                )
+                    # Get all test before first white space
+                    website_url_text = website_url_text.split(" ")[0]
 
-                with open(data_file_str, "a") as data_file:
-                    data_file.write(
-                        f"{product},{website_name_text},{website_url_text}\n"
+                    # Parse URL to find origin
+                    parsed_url = urlparse(website_url_text)
+
+                    # Check if URL is forbidden
+                    if parsed_url.netloc in FORBIDDEN_SHOPS:
+                        continue
+
+                    # Check if URL is not .nl or .com
+                    if not parsed_url.netloc.endswith(
+                        ".nl"
+                    ) and not parsed_url.netloc.endswith(".com"):
+                        continue
+
+                    # Check if URL was found already to prevent doubles
+                    if website_url_text in website_url_history:
+                        continue
+
+                    website_url_history.append(website_url_text)
+                    result.append(
+                        {
+                            "product": product,
+                            "company": website_name_text,
+                            "website": website_url_text,
+                        }
                     )
 
-            page_count += 1
-        except Exception as e:
-            print(f"Error: {e}")
-            continue
+                    with open(data_file_str, "a") as data_file:
+                        data_file.write(
+                            f"{product},{website_name_text},{website_url_text}\n"
+                        )
 
-    return result
+                page_count += 1
+            except Exception as e:
+                print(f"Error: {e}")
+                continue
+
+        return result
+
+    # Get all products
+    products = []
+
+    # Get kind of products
+    with open("producten.txt", "r") as file:
+        for line in file:
+            products.append(line.strip())
+
+    # Remove duplicates
+    products = list(set(products))
+
+    # Start Playwright
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+            locale="nl-NL",
+        )
+        page = context.new_page()
+        page.wait_for_timeout(600000)
+
+        # Iterate over all products
+        for index, product in enumerate(products):
+            # Print current product
+            print(f"Searching for {product}... ({index + 1} of {len(products)})")
+
+            # Perform Google search
+            google_search(page, product)
+
+            # Random sleep to avoid being blocked
+            time.sleep(random.uniform(2, 5))
+
+        browser.close()
 
 
-products = []
-
-# Get kind of products
-with open("producten.txt", "r") as file:
-    for line in file:
-        products.append(line.strip())
-
-# Remove duplicates
-products = list(set(products))
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    context = browser.new_context(
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-        locale="nl-NL",
-    )
-    page = context.new_page()
-
-    for index, product in enumerate(products):
-        print(f"Searching for {product}... ({index + 1} of {len(products)})")
-
-        # Perform Google search
-        links = google_search(page, product)
-
-        # Random sleep to avoid being blocked
-        time.sleep(random.uniform(2, 5))
-
-    browser.close()
+if __name__ == "__main__":
+    get_shops()
